924d871b7239fefa6a8ea53686ead3bcc005cfcd
mystic-repo <13512073+mystic-repo@users.noreply.github.com>
2022-01-23 12:56:21 -0800

    added docker python helper

--- test/test_main.py
+++ test/test_main.py
@@ -6,15 +6,17 @@ def test_main():
 
 	_libsecp256k1 = None
 	libsecp256k1 = Libsecp265k1()
-	try:
-		_libsecp256k1 = libsecp256k1.load_library()
+	loader = libsecp256k1.load_library()
+	print(loader)
+#	try:
+#		_libsecp256k1 = 
 		#print(libsecp256k1.win32())
-	except BaseException as e:
-		print(repr(e))
+#	except BaseException as e:
+#		print(repr(e))
 
-	if _libsecp256k1 is None:
+#	if _libsecp256k1 is None:
 		# hard fail:
-		sys.exit(f"Error: Failed to load libsecp256k1.")
+#		sys.exit(f"Error: Failed to load libsecp256k1.")
 
 if __name__ == '__main__':
 	raise SystemExit(test_main())
