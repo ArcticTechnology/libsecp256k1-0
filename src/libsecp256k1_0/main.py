5dedd030b3464b684282253c5a47e14bbe818497
ArcticTechnology <13512073+ArcticTechnology@users.noreply.github.com>
2023-02-20 19:58:06 -0800

    update main to double quotes

--- src/libsecp256k1_0/main.py
+++ src/libsecp256k1_0/main.py
@@ -4,5 +4,5 @@ from .libsecp256k1 import Libsecp256k1
 def main():
 	Libsecp256k1.docker_compile(interactive=True, sudo=True)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	raise SystemExit(main())
--- test/test_main.py
+++ test/test_main.py
@@ -4,5 +4,5 @@ from libsecp256k1_0 import Secp256k1
 def test_main():
 	print(Secp256k1._load)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	raise SystemExit(test_main())
